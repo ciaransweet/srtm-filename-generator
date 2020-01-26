da0d5d4767a798f72d0464c3414e39aed9453a6f
Ciaran Evans <9111975+ciaranevans@users.noreply.github.com>
2020-01-26 17:15:18 +0000

    Add further tests

--- src/test_filename_generator.py
+++ src/test_filename_generator.py
@@ -5,7 +5,7 @@ import filename_generator
 
 class Tests(unittest.TestCase):
 
-    def test_all_positive_north_east(self):
+    def test_north_east(self):
         btm_lat = 1.5
         top_lat = 3.5
         left_lon = 1.5
@@ -16,7 +16,18 @@ class Tests(unittest.TestCase):
         filenames = filename_generator.main(btm_lat, top_lat, left_lon, right_lon)
         self.assertListEqual(filenames, expected_filenames)
 
-    def test_all_negative_south_west(self):
+    def test_north_west(self):
+        btm_lat = 1.5
+        top_lat = 3.5
+        left_lon = -2.5
+        right_lon = -0.5
+        expected_filenames = ['N01W003.hgt', 'N01W002.hgt', 'N01W001.hgt',
+                              'N02W003.hgt', 'N02W002.hgt', 'N02W001.hgt',
+                              'N03W003.hgt', 'N03W002.hgt', 'N03W001.hgt']
+        filenames = filename_generator.main(btm_lat, top_lat, left_lon, right_lon)
+        self.assertListEqual(filenames, expected_filenames)
+
+    def test_south_west(self):
         btm_lat = -10.5
         top_lat = -7.5
         left_lon = -120.5
@@ -26,6 +37,18 @@ class Tests(unittest.TestCase):
                               'S09W121.hgt', 'S09W120.hgt', 'S09W119.hgt', 'S09W118.hgt',
                               'S08W121.hgt', 'S08W120.hgt', 'S08W119.hgt', 'S08W118.hgt']
         filenames = filename_generator.main(btm_lat, top_lat, left_lon, right_lon)
+        self.assertListEqual(filenames, expected_filenames)
+
+    def test_south_east(self):
+        btm_lat = -10.5
+        top_lat = -7.5
+        left_lon = 117.5
+        right_long = 120.5
+        expected_filenames = ['S11E117.hgt', 'S11E118.hgt', 'S11E119.hgt', 'S11E120.hgt',
+                              'S10E117.hgt', 'S10E118.hgt', 'S10E119.hgt', 'S10E120.hgt',
+                              'S09E117.hgt', 'S09E118.hgt', 'S09E119.hgt', 'S09E120.hgt',
+                              'S08E117.hgt', 'S08E118.hgt', 'S08E119.hgt', 'S08E120.hgt']
+        filenames = filename_generator.main(btm_lat, top_lat, left_lon, right_long)
         self.assertListEqual(filenames, expected_filenames)
 
     def test_equator_cross_east(self):
